3cc068cc4e0c7c772f72440c575e89d520c5a7e4
yairnadler <79379887+yairnadler@users.noreply.github.com>
2024-04-14 11:13:39 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,14 +2,13 @@ const express = require("express");
 const cors = require("cors");
 const { CodeBlocks } = require("./model/CodeBlocks");
 const { Server } = require("socket.io");
-const baseUrl = "https://online-code-editor-client.vercel.app"
+const { baseUrl } = require("./constants");
 
 const app = express();
 const PORT = 3080;
 
 const corsOptions = {
   origin: `${baseUrl.client}`,
-  credentials: true,
 };
 
 const io = new Server({ cors: corsOptions });
